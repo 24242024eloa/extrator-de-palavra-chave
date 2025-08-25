7851bd2ba327623fae9c083dda773f5ac448fbdc
luana <luana.fatima.chaves@escola.pr.gov.br>
2025-08-25 08:51:35 -0300

    hkujgjkbkl

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const botaoMostraPalavras = documento.querySelector(#botao-palavrachave");
+const botaoMostraPalavras = documento.querySelector("#botao-palavrachave");
 botaoMostraPalavra.addEventListener("click",mostraPalavraChave);
-fuction mostraPalavraChave() {
-    const texto = documento.querySelector(#entrada-de-texto").valeu;
-    const campoResultado = documento.querySelector(#resultado-palavrachave");
+function mostraPalavraChave(){
+    const texto= documento.querySelector("#entrada-de-texto").valeu;
+    const campoResultado = documento.querySelector("#resultado-palavrachave");
     const palavraChave = processaTexto(texto);
     campoResultado.texContent = palavraChave.join(".");
 }
