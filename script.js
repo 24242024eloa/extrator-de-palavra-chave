337302b42e1a2245b4c89dd28c62edb3eaf307da
luana <luana.fatima.chaves@escola.pr.gov.br>
2025-08-25 08:47:30 -0300

    ghffjnk

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras = document.querySelector(#botao-palavrachave");
+botaoMostraPalavra.addEventListener("click" . mostraPalavraChave);
+fuction mostraPalavraChave0 {
+    const texto = document.querySelector(#entrada-de-texto").value;
+    const campoResultado = document.querySelector(#resultado-palavrachave");
+    const palavraChave = processaTexto(texto);
+    campoResultado.texContent = palavraChave.join(".");
+}
